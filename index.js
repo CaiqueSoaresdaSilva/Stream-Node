fabdf44576dc4a593b824bd5216888f72eafe0b3
caique.soares@trilobitglobal.com <caique.soares@trilobitglobal.com>
2021-11-16 17:35:23 -0300

    Finalizado Streaming RTSP usando NodeJS

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const app = require("express")();
-
+const cors = require('cors');
 const Stream = require("node-rtsp-stream");
 
+app.use(cors());
 const streams = {};
 
 const stream_configs = [{
-    key: 'Portaria',
-    port: 9000,
-    url: 'rtsp://admin:Tri123456@192.168.17.12/ISAPI/Streaming/Channels/101.mov'
-}];
+        key: 'Portaria',
+        port: 9000,
+        url: 'rtsp://admin:Tri123456@192.168.17.12/ISAPI/Streaming/Channels/101.m3u'
+    }
+];
+
+const stopStream = (port) => {
+    if (streams[port]){
+        streams[port].stop();
+        streams[port] = null;
+    }
+};
 
 const startStream = (name, streamUrl, wsPort) =>{
 
@@ -23,9 +32,9 @@ const startStream = (name, streamUrl, wsPort) =>{
     });
 
     streams[wsPort] = stream;
-}
+};
 
-app.get('start-Stream', (req, res) => {
+app.get("/start-stream", (req, res) => {
     // URL = RTSP LINNK
     // PORT = WSPORT
     const {url, port, key = "stream"} = req.query;
@@ -48,9 +57,23 @@ app.get('start-Stream', (req, res) => {
 
 });
 
+app.get("/stop-stream", (req, res) => {
+
+    const { port } = req.query;
+    if(!streams[port]){
+        return res.json({
+            message: "Port is not in use",
+        });
+    }
+    stopStream(port);
+
+    return res.json({
+        message: "Stopped"
+    });
+});
+
 app.listen(8080, () => {
     console.log('Server running 8080');
-    
     stream_configs.forEach((config)=>{
         startStream(config.key, config.url, config.port);
     })
